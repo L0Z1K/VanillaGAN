36feea16e637e0c3610f034f34ae3d34d6d5c6ac
L0Z1K <qortmddbs731@korea.ac.kr>
2020-10-12 21:09:08 +0900

    complete assignment

--- README.md
+++ README.md
@@ -1,2 +1,25 @@
 # VanillaGAN
-GAN for MNIST
+- GAN for MNIST
+
+
+
+### Results
+
+![image](result/result.gif)
+
+
+
+#### Epoch 190
+
+![image](result/result.png)
+
+
+
+### References
+
+https://papers.nips.cc/paper/5423-generative-adversarial-nets.pdf
+
+---
+
+
+
--- main.py
+++ main.py
@@ -72,19 +72,20 @@ optim_D = torch.optim.Adam(D.parameters(), lr=0.0001)
 criterion = nn.BCELoss()
 
 print("[+] Train Start")
-total_epochs = 100
+total_epochs = 200
 total_batch = len(data_train)
 for epoch in range(total_epochs):
     avg_cost = [0, 0]
-    for i, (x,_) in enumerate(data_train):
+    for x, _ in data_train:
         real = (torch.FloatTensor(x.size(0), 1).fill_(1.0)).to(device)
         fake = (torch.FloatTensor(x.size(0), 1).fill_(0.0)).to(device)
         
         x = x.view(x.size(0), -1).to(device)
 
-        noise = torch.rand(batch_size, 100).to(device)
+        noise = torch.randn(batch_size, 100, device=device)
 
         fake_img = G(noise)
+
         # Train Generator
         optim_G.zero_grad()
         g_cost = criterion(D(fake_img), real)
@@ -94,19 +95,19 @@ for epoch in range(total_epochs):
         fake_img = fake_img.detach().to(device)
         # Train Discriminator
         optim_D.zero_grad()
-        y_pred= torch.cat((x, fake_img))
-        y_pred = D(y_pred)
-        y = torch.cat((real, fake))
-        d_cost = criterion(y_pred, y)
+        d_cost = criterion(D(torch.cat((x, fake_img))), torch.cat((real, fake)))
         d_cost.backward()
         optim_D.step()
 
         avg_cost[0] += g_cost
         avg_cost[1] += d_cost
+    
     avg_cost[0] /= total_batch
     avg_cost[1] /= total_batch
-    print("Epoch: %d, Generator: %f, Discriminator: %f"%(epoch+1, avg_cost[0], avg_cost[1]))
+
     
-    fake_img = fake_img.reshape([100, 1, 28, 28])
-    img_grid = make_grid(fake_img, nrow=10, normalize=True)
-    save_image(img_grid, "image/%d.png"%(epoch+1))
+    if (epoch+1) % 10 == 0:
+      fake_img = fake_img.reshape([batch_size, 1, 28, 28])
+      img_grid = make_grid(fake_img, nrow=10, normalize=True)
+      save_image(img_grid, "result/%d.png"%(epoch+1))
+      print("Epoch: %d, Generator: %f, Discriminator: %f"%(epoch+1, avg_cost[0], avg_cost[1]))
